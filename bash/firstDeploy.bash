271d98fe3fb8c79875799e9ccf34ab0de3af6213
CatScan <57345237+CatScan-crypt@users.noreply.github.com>
2024-12-01 17:17:50 +0200

    Update firstDeploy.bash

--- bash/firstDeploy.bash
+++ bash/firstDeploy.bash
@@ -5,13 +5,13 @@ cp example.env .env
 
 # Prompt the user to enter the DEPLOYER_PRIVATE_KEY
 echo "Please enter your wallet's private key:"
-read -s deployer_private_key
+read -r -s deployer_private_key
 # Append the DEPLOYER_PRIVATE_KEY to the .env file
 sed -i "s|DEPLOYER_PRIVATE_KEY=.*|DEPLOYER_PRIVATE_KEY=$deployer_private_key|" .env
 
 # Prompt the user to enter the DEPLOYER_ADDRESS
 echo "Please enter your wallet's address:"
-read deployer_address
+read -r deployer_address
 # Append the DEPLOYER_ADDRESS to the .env file
 sed -i "s|DEPLOYER_ADDRESS=.*|DEPLOYER_ADDRESS=$deployer_address|" .env
 
