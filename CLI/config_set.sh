b02bb82d146b12b7252f18d7b1e953bee23d78bd
CatScn <mariroth62@gmail.com>
2025-03-02 14:10:04 +0200

    Update config_set.sh

--- CLI/config_set.sh
+++ CLI/config_set.sh
@@ -11,14 +11,15 @@ load_env() {
   source "$ENV_FILE"
 }
 
-# Load environment variables
-load_env
 
 # Parse flags
-while getopts "g:" opt; do
+while getopts "c:g:" opt; do
   case $opt in
+    c)
+      config_action="$OPTARG"
+      ;;
     g)
-      set_option="$OPTARG"
+      get_option="$OPTARG"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -31,36 +32,59 @@ while getopts "g:" opt; do
   esac
 done
 
-# Check if -g is used
-if [ -n "$set_option" ]; then
-  read -p "Enter new value for $set_option: " new_value
-  case "$set_option" in
+# Check if --config and --get are used together
+if [ -n "$config_action" ] && [ -n "$get_option" ]; then
+  case "$get_option" in
     "private-key")
-      sed -i "s/^DEPLOYER_PRIVATE_KEY=.*/DEPLOYER_PRIVATE_KEY=\"$new_value\"/" "$ENV_FILE"
+      echo "$DEPLOYER_PRIVATE_KEY"
+  case "$config_action" in
+    "get")
+      case "$get_option" in
+        "private-key")
+          echo "$DEPLOYER_PRIVATE_KEY"
+          ;;
+        "account-address")
+          echo "$DEPLOYER_ADDRESS"
+          ;;
+        "network")
+          echo "$NETWORK"
+          ;;
+        "rpc-endpoint-sepolia")
+          echo "$RPC_ENDPOINT_SEPOLIA"
+          ;;
+        "rpc-endpoint-mainnet")
+          echo "$RPC_ENDPOINT_MAINNET"
+          ;;
+        *)
+          echo "Invalid get option: $get_option"
+          exit 1
+          ;;
+      esac
       ;;
     "account-address")
-      sed -i "s/^DEPLOYER_ADDRESS=.*/DEPLOYER_ADDRESS=\"$new_value\"/" "$ENV_FILE"
+      echo "$DEPLOYER_ADDRESS"
       ;;
     "network")
-      sed -i "s/^NETWORK=.*/NETWORK=\"$new_value\"/" "$ENV_FILE"
+      echo "$NETWORK"
       ;;
     "rpc-endpoint-sepolia")
-      echo "Cannot set rpc-endpoint-sepolia"
-      exit 1
+      echo "$RPC_ENDPOINT_SEPOLIA"
       ;;
     "rpc-endpoint-mainnet")
-      echo "Cannot set rpc-endpoint-mainnet"
+      echo "$RPC_ENDPOINT_MAINNET"
+    "init")
+      echo "Init not implemented yet"
       exit 1
       ;;
     *)
-      echo "Invalid set option: $set_option"
+      echo "Invalid get option: $get_option"
+      echo "Invalid config action: $config_action"
       exit 1
       ;;
   esac
-  echo "$set_option set to $new_value"
 else
-  echo "Usage: ./config_set.sh -g <option>"
+  echo "Usage: ./cli.sh -c <config_option> -g <get_option>"
+  echo "Usage: ./config_get.sh -c get -g <option>"
   echo ""
   echo "Config Options: private-key, account-address, network, rpc-endpoint-sepolia, rpc-endpoint-mainnet"
   exit 1
-fi
