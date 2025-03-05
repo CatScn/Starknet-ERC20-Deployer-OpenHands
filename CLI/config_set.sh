997213fa744a770ee350219853353e769dce5a8c
CatScn <mariroth62@gmail.com>
2025-03-05 12:31:07 +0200

    Update config_set.sh

--- CLI/config_set.sh
+++ CLI/config_set.sh
@@ -1,38 +1,26 @@
 #!/usr/bin/env bash
-
-# Determine the script's directory
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
-
-# Construct the absolute path to the .env files
 ENV_FILE="$SCRIPT_DIR/../.env"
-
-# Determine which env file to use
-ENV_TO_SOURCE="$ENV_FILE"
 
 # Load environment variables
 load_env() {
-  if [ -f "$ENV_TO_SOURCE" ]; then
-    source "$ENV_TO_SOURCE"
-  else
-    echo "Error: Environment file '$ENV_TO_SOURCE' not found."
-    echo "Creating a new .env file."
-    "$SCRIPT_DIR/config_init.sh"
-    exit 1
-  fi
+  source "$ENV_FILE"
 }
 
 # Load environment variables
 load_env
 
 # Parse flags
-while getopts "o:" opt; do
+while getopts "g:" opt; do
   case $opt in
-    o)
-      get_option="$OPTARG"
+    g)
+      option_and_value="$OPTARG"
+      option="${option_and_value%=*}"
+      value="${option_and_value#*=}"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
-      exit 1
+      show_help=true
       ;;
     :)
       echo "Option -$OPTARG requires an argument." >&2
@@ -40,57 +28,36 @@ while getopts "o:" opt; do
       ;;
   esac
 done
-message="DEPLOYER_PRIVATE_KEY, DEPLOYER_ADDRESS, NETWORK, RPC_ENDPOINT_SEPOLIA, RPC_ENDPOINT_MAINNET, TOKEN_NAME, SYMBOL_NAME, DECIMALS_LENGTH, FIXED_SUPPLY"
-helpMessage="$message"
 
-# Check if -o is used
-if [ -n "$get_option" ]; then
-  case "$get_option" in
-    "DEPLOYER_PRIVATE_KEY")
-      printf "%s\n" "${DEPLOYER_PRIVATE_KEY}"
-      exit 0
-      ;;
-    "DEPLOYER_ADDRESS")
-      printf "%s\n" "${DEPLOYER_ADDRESS}"
-      exit 0
-      ;;
-    "NETWORK")
-      printf "%s\n" "${NETWORK}"
-      exit 0
-      ;;
-    "RPC_ENDPOINT_MAINNET")
-      printf "%s\n" "${RPC_ENDPOINT_MAINNET}"
-      exit 0
-      ;;
-    "RPC_ENDPOINT_SEPOLIA")
-      printf "%s\n" "${RPC_ENDPOINT_SEPOLIA}"
-      exit 0
-      ;;
-    "TOKEN_NAME")
-      printf "%s\n" "${TOKEN_NAME}"
-      exit 0
-      ;;
-    "SYMBOL_NAME")
-      printf "%s\n" "${SYMBOL_NAME}"
-      exit 0
-      ;;
-    "DECIMALS_LENGTH")
-      printf "%s\n" "${DECIMALS_LENGTH}"
-      exit 0
-      ;;
-    "FIXED_SUPPLY")
-      printf "%s\n" "${FIXED_SUPPLY}"
-      exit 0
-      ;;
-    *)
-      echo "Invalid get option: $get_option"
-      echo "$helpMessage"
-      exit 1
-      ;;
-  esac
-else
-  echo "Usage: ./config_get.sh -o <option>"
+# Set config option
+case "$option" in
+  "private-key")
+    sed -i "s/^DEPLOYER_PRIVATE_KEY=.*/DEPLOYER_PRIVATE_KEY=$value/" "$ENV_FILE"
+    ;;
+  "account-address")
+    sed -i "s/^DEPLOYER_ADDRESS=.*/DEPLOYER_ADDRESS=$value/" "$ENV_FILE"
+    ;;
+  "network")
+    sed -i "s/^NETWORK=.*/NETWORK=$value/" "$ENV_FILE"
+    ;;
+  "rpc-endpoint-sepolia")
+    sed -i "s/^RPC_ENDPOINT_SEPOLIA=.*/RPC_ENDPOINT_SEPOLIA=$value/" "$ENV_FILE"
+    ;;
+  "rpc-endpoint-mainnet")
+    sed -i "s/^RPC_ENDPOINT_MAINNET=.*/RPC_ENDPOINT_MAINNET=$value/" "$ENV_FILE"
+    ;;
+  *)
+    echo "Invalid option: $option"
+    show_help=true
+    ;;
+esac
+
+if [ "$show_help" = true ]; then
   echo ""
-  echo "$helpMessage"
+  echo "Usage: ./config_set.sh -g <option>=value"
+  echo "Config Options: private-key, account-address, network, rpc-endpoint-sepolia, rpc-endpoint-mainnet"
+  echo ""
   exit 1
 fi
+
+load_env # Reload environment variables after modification
